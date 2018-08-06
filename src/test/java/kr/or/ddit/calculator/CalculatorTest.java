1810e02d15853ba0232e2c20965ee46971f2e543
rlaakdma <rlaakdma@hanmail.net>
2018-08-06 15:13:04 +0900

    commit

--- src/test/java/kr/or/ddit/calculator/CalculatorTest.java
+++ src/test/java/kr/or/ddit/calculator/CalculatorTest.java
@@ -3,9 +3,7 @@ package kr.or.ddit.calculator;
 import static org.junit.Assert.*;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
